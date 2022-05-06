313c07542fcda314a21c3d84b6def66245b5a907
john1223ihji8n <99758624+john1223ihji8n@users.noreply.github.com>
2022-05-06 13:54:38 -0500

    Archivos iniciales del proyecto de MakeCode

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Código generado automáticamente. No editar.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "bizo":return img`
+. . . . . . . . . . . . . . . . 
+. . . f f f f f f f f f f . . . 
+. f f f 4 4 4 4 4 4 4 4 f f f . 
+. f 4 4 4 4 4 4 4 4 4 4 4 4 f . 
+. f 4 4 4 5 5 5 5 5 5 4 4 4 f . 
+. f 4 5 5 6 6 6 6 6 5 5 5 4 f . 
+. f 4 5 6 1 6 1 6 6 6 6 5 4 f . 
+. f 4 5 6 6 1 6 6 6 6 6 5 4 f . 
+. f 4 5 6 1 6 1 6 6 6 6 5 4 f . 
+. f 4 5 6 f 6 6 6 6 f 6 5 4 f . 
+. f 4 5 6 6 6 6 6 6 6 6 5 4 f . 
+. f 4 5 6 6 f f f f 6 6 5 4 f . 
+. f 4 5 6 6 f f f f 6 6 5 4 f . 
+. f 4 5 6 6 6 6 6 6 6 6 5 4 f . 
+. f 4 5 5 5 5 5 5 5 5 5 5 4 f . 
+. f f f f f f f f f f f f f f . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
---
+++ main.ts
@@ -0,0 +1,4 @@
+let juan = sprites.create(assets.image`bizo`, SpriteKind.Player)
+controller.moveSprite(juan)
+scene.cameraFollowSprite(juan)
+let nivelactual = 0
